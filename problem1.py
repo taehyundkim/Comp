03e1eb73cf6c669e530a3f44b4b5409e84a90d17
ArihantJ <arihantjaingoa@gmail.com>
2023-01-21 15:20:11 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -4,7 +4,7 @@ lst_creeper = input().split(" ")
 
 gcf = int(lst_creeper[0])
 for i in range(0, len(lst_creeper) - 1):
-    gcf = math.gcd(gcf, int(lst_creeper[i+1]))
+    gcf = math.gcd(gcf, int(lst_creeper[i + 1]))
     
 print(gcf)
     
