750d86910b61c3f2b9b9b784f0989d96fd457dd7
ArihantJ <arihantjaingoa@gmail.com>
2023-01-21 15:17:08 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,10 @@
-lst_creeper = int(input().split(" "))
+import math
+
+lst_creeper = input().split(" ")
+
+gcf = int(lst_creeper[0])
+for i in range(0, len(lst_creeper) - 1):
+    gcf = math.gcd(gcf, int(lst_creeper[i+1]))
+    
+print(gcf)
+    
