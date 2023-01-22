024f899380cd293cdb19cc5d7bcccb7d6208e40f
ArihantJ <arihantjaingoa@gmail.com>
2023-01-21 17:05:04 -0800

    Update problem5.py

--- problem5.py
+++ problem5.py
@@ -2,22 +2,27 @@ delay = int(input())
 wither = int(input())
 n = int(input())
 
+
+#flower = [3, 4, 5, 6, 6]
+
+
+
 flowers = [1]
-count = 1
+
 
 for i in range(1, n+1):
     #flow_copy = flowers.copy()
     for j in flowers:
         if (i - j) > 0 and (i - j) % wither == 0:
-            count -= 1
+            #count -= 1
             flowers.remove(j)
             #print("day of death:" + str(i))
         if (i - j) > 0 and (i - j) >= delay:
-            count += 1
+            #count += 1
             #print(" day of birth:" + str(i))
             flowers.append(i)
         
     #flowers = flow_copy
     #print("count: " + str(count))
     
-print(count)
+print(len(flowers))
