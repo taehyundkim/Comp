3e9323a2ec84ebc0e75e878bf337108fb6380c10
ArihantJ <arihantjaingoa@gmail.com>
2023-01-21 17:00:33 -0800

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -4,4 +4,7 @@ sticks = [int(element) for element in input().strip().split()]
 # TODO
 sticks.sort()
 
-print(max(sticks[0] + sticks[-1], sticks[n//2] + sticks[n//2 + 1]))
+if n % 2 == 0:
+    print(max((sticks[0] + sticks[-1]), (sticks[n//2] + sticks[n//2 - 1])))
+else:
+    print(max((sticks[0] + sticks[-1]), (sticks[n//2] + sticks[n//2 + 1])))
